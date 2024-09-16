89d256c6edc176237d8e49272eba17585707b3cd
Fredrik Klingenberg <fredrkl@gmail.com>
2024-09-16 12:16:38 +0200

    ✨ Records examples
    Signed-off-by: Fredrik Klingenberg <fredrkl@gmail.com>

--- Person.cs
+++ Person.cs
@@ -2,7 +2,7 @@ namespace MyNamespace {
 
   public class Person
   {
-      public string Name { get; set; } = "";
+      public string Name { get; init; } = "";
       public int Age { get; set; }
   }
 
---
+++ Records.cs
@@ -0,0 +1,6 @@
+public record Car()
+{
+    public string Make { get; init; } = string.Empty;
+    public string Model { get; init; } = string.Empty;
+    public int Year { get; init; } = 0;
+}

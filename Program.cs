2edf48d66d2f8d8dee52a227bc3b8fec38c914c2
Fredrik Klingenberg <fredrkl@gmail.com>
2024-09-20 07:55:01 +0200

    ♻️  Move into several catalogs
    Signed-off-by: Fredrik Klingenberg <fredrkl@gmail.com>

--- Program.cs
+++
@@ -1,2 +0,0 @@
-﻿var something = new Car{ Make = "Opel", Model="Ampera", Year=2003 };
-var another = something with { Year = 2006 };
---
+++ records-explorer/Program.cs
@@ -0,0 +1,7 @@
+﻿var something = new Car{ Make = "Opel", Model="Ampera", Year=2003 };
+var another = something with { Year = 2006 };
+Console.WriteLine(another.Make); // Output: Opel
+var x = 4;
+var y = x + 5;
+var z = y + 6;
+Console.WriteLine("Appelsin"); // Output: Opel

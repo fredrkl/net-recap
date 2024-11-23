34ccf555db1bd3d0635c4178418baa6ecc1300d5
Fredrik Klingenberg <fredrkl@gmail.com>
2024-11-23 19:32:39 +0100

    ✨ (feature-explorer): strong configuration binding
    Signed-off-by: Fredrik Klingenberg <fredrkl@gmail.com>

---
+++ src/feature-explorer/Configurations.cs
@@ -0,0 +1,6 @@
+// See https://aka.ms/new-console-template for more information
+//using Microsoft.Extensions.Hosting;
+public class Configurations
+{
+  public bool preview { get; set; }
+}
--- src/feature-explorer/Program.cs
+++ src/feature-explorer/Program.cs
@@ -6,9 +6,13 @@ Console.WriteLine("Hello, World!");
 
 IConfiguration configuration = new ConfigurationBuilder()
     .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json", optional: true, reloadOnChange: true)
     .Build();
 
+var appSettings = configuration.GetSection("Configurations").Get<Configurations>();
+
 Console.WriteLine($"The value of 'key1' is '{configuration["key1"]}'");
+Console.WriteLine($"The value of 'Preview' is '{appSettings?.preview}'");
 
 //     .ConfigureServices((context, services) =>
 //     {

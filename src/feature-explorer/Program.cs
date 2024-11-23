76db4dced9d5c2197f7666daef9c37a5279da2e8
Fredrik Klingenberg <fredrkl@gmail.com>
2024-11-23 12:43:40 +0100

    ✨ Feature explorer
    Feature toogle and configuration settings
    
    Signed-off-by: Fredrik Klingenberg <fredrkl@gmail.com>

---
+++ src/feature-explorer/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+//using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+
+Console.WriteLine("Hello, World!");
+
+IConfiguration configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+    .Build();
+
+Console.WriteLine($"The value of 'key1' is '{configuration["key1"]}'");
+
+//     .ConfigureServices((context, services) =>
+//     {
+//         services.AddFeatureExplorer();
+//     })
+
+//var host = Host.CreateDefaultBuilder(args)
+//    .ConfigureServices((context, services) =>
+//    {
+//        services.AddFeatureExplorer();
+//    })
+//    .Build();

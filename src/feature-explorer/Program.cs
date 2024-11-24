83a69d489d7c53f78724d6cc4d5a24de94dbfba7
Fredrik Klingenberg <fredrkl@gmail.com>
2024-11-24 16:32:10 +0100

    ✨ (feature-explorer): showcasing feature flags
    Signed-off-by: Fredrik Klingenberg <fredrkl@gmail.com>

--- src/feature-explorer/Program.cs
+++ src/feature-explorer/Program.cs
@@ -4,18 +4,25 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.FeatureManagement;
 
 Console.WriteLine("Hello, World!");
 
+// Configuration
 IConfiguration configuration = new ConfigurationBuilder()
   .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
   .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json", optional: true, reloadOnChange: true)
   .Build();
 
-  var appSettings = configuration.GetSection("Configurations").Get<Configurations>();
+// Services with DI
+// IServiceCollection services_already = new ServiceCollection();
+// services_already.AddLogging();
+// services_already.AddTransient<IMyClass, MyClass>();
 
-  Console.WriteLine($"The value of 'key1' is '{configuration["key1"]}'");
-  Console.WriteLine($"The value of 'Preview' is '{appSettings?.preview}'");
+var appSettings = configuration.GetSection("Configurations").Get<Configurations>();
+
+Console.WriteLine($"The value of 'key1' is '{configuration["key1"]}'");
+Console.WriteLine($"The value of 'Preview' is '{appSettings?.preview}'");
 
 var host = Host.CreateDefaultBuilder(args)
   .ConfigureAppConfiguration((hostingContext, config) =>
@@ -25,6 +32,7 @@ var host = Host.CreateDefaultBuilder(args)
   .ConfigureServices((hostContext, services) => {
     services.AddLogging();
     services.AddTransient<IMyClass, MyClass>();
+    services.AddFeatureManagement();
     })
   .ConfigureLogging(logging => {
       logging.AddConsole();
@@ -37,5 +45,13 @@ var host = Host.CreateDefaultBuilder(args)
 
   logger.LogInformation("This is an informational message.");
   logger.LogInformation("This is an informational message for {name}.", nameof(logger));
+
+  var feature_manager = host.Services.GetRequiredService<IFeatureManager>();
+  var enabledA = await feature_manager.IsEnabledAsync("FeatureA");
+  var enabledB = await feature_manager.IsEnabledAsync("FeatureB");
+
+  logger.LogInformation("The feature FeatureA is enabled: {enabled}", enabledA);
+  logger.LogInformation("The feature FeatureB is enabled: {enabled}", enabledB);
+
 
   host.Run();
